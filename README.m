e3082f33d4db9610f5763c201218b0652370f633
Syed Mohsin <syedm.90@gmail.com>
2017-06-17 19:16:30 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,2 @@
+[![Build Status](https://img.shields.io/travis/syed-mohsin/react-redux-boilerplate.svg?style=flat-square)](https://travis-ci.org/syed-mohsin/react-redux-boilerplate)
+[![Coverage Status](https://img.shields.io/coveralls/syed-mohsin/react-redux-boilerplate.svg?style=flat-square)](https://coveralls.io/github/syed-mohsin/react-redux-boilerplate?branch=master)
