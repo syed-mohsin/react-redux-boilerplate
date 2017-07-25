6b3e89b1a483b44c9a4b5e12a245ab8d3366912c
Syed Mohsin <syedm.90@gmail.com>
2017-07-25 14:14:55 -0700

    add default value for spreadsheet env

--- src/server/spreadsheet.js
+++ src/server/spreadsheet.js
@@ -1,16 +1,16 @@
 import GoogleSpreadsheet from 'google-spreadsheet'
 
 const credentials = {
-  type: process.env.GOOGLE_SHEETS_TYPE,
-  project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_SHEETS_AUTH_URI,
-  token_uri: process.env.GOOGLE_SHEETS_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_SHEETS_CLIENT_X509_CERT_URL,
+  type: process.env.GOOGLE_SHEETS_TYPE || 'GOOGLE_SHEETS_TYPE',
+  project_id: process.env.GOOGLE_SHEETS_PROJECT_ID || 'GOOGLE_SHEETS_PROJECT_ID',
+  private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID || 'GOOGLE_SHEETS_PRIVATE_KEY_ID',
+  private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n') || 'GOOGLE_SHEETS_PRIVATE_KEY',
+  client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL || 'GOOGLE_SHEETS_CLIENT_EMAIL',
+  client_id: process.env.GOOGLE_SHEETS_CLIENT_ID || 'GOOGLE_SHEETS_CLIENT_ID',
+  auth_uri: process.env.GOOGLE_SHEETS_AUTH_URI || 'GOOGLE_SHEETS_AUTH_URI',
+  token_uri: process.env.GOOGLE_SHEETS_TOKEN_URI || 'GOOGLE_SHEETS_TOKEN_URI',
+  auth_provider_x509_cert_url: process.env.GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL || 'GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL',
+  client_x509_cert_url: process.env.GOOGLE_SHEETS_CLIENT_X509_CERT_URL || 'GOOGLE_SHEETS_CLIENT_X509_CERT_URL',
 }
 
 
